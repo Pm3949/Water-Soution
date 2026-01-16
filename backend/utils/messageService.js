e72065807383ffc4ce0a2c04f59bd1900651bd21
Pm3949 <manavpatel0767@email.com>
2026-01-16 21:52:57 +0530

    Frontend Ready4

--- backend/utils/messageService.js
+++ backend/utils/messageService.js
@@ -1,15 +1,24 @@
 import axios from "axios";
 
+const formatPhone = (phone) => {
+  if (!phone.startsWith("+")) {
+    return `+91${phone}`;
+  }
+  return phone;
+};
+
+
 export const sendWhatsapp = async (phone, name) => {
   try {
     const sid = process.env.TWILIO_SID;
     const token = process.env.TWILIO_AUTH_TOKEN;
 
     const url = `https://api.twilio.com/2010-04-01/Accounts/${sid}/Messages.json`;
-
+    const toPhone = formatPhone(phone);
+    
     const data = new URLSearchParams({
       From: process.env.TWILIO_WHATSAPP_FROM,
-      To: `whatsapp:${phone}`,
+      To: `whatsapp:${toPhone}`,
       Body: `Hello ${name}, your RO service is due. Please call ${process.env.APP_PHONE} to book service.`,
     });
 
@@ -32,10 +41,11 @@ export const sendSMS = async (phone, name) => {
     const sid = process.env.TWILIO_SID;
     const token = process.env.TWILIO_AUTH_TOKEN;
     const url = `https://api.twilio.com/2010-04-01/Accounts/${sid}/Messages.json`;
+     const toPhone = formatPhone(phone);
 
     const data = new URLSearchParams({
         From: process.env.TWILIO_SMS_FROM,
-        To: `${phone}`,
+              To: toPhone,
         Body: `Hello ${name}, your RO service is due. Please call ${process.env.APP_PHONE} to book service.`,
     });
 
